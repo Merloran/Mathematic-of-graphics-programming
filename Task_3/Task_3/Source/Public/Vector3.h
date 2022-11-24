3e00d10865330b732a91ccfc13edda57d74a6dc3
Merloran <poncyleuszmaciej@gmail.com>
2022-11-24 09:06:35 +0100

    Complete task 3

---
+++ Task_3/Task_3/Source/Public/Vector3.h
@@ -0,0 +1,188 @@
+#pragma once
+#include "corecrt_math_defines.h"
+#include <iostream>
+#include <cmath>
+
+template <typename Type>
+class Vector3
+{
+public:
+	Type X, Y, Z;
+
+	Vector3(Type X, Type Y, Type Z)
+		: X(X)
+		, Y(Y)
+		, Z(Z)
+	{}
+
+	Vector3(Type Value)
+		: X(Value)
+		, Y(Value)
+		, Z(Value)
+	{}
+
+	Vector3()
+		: X((Type)0)
+		, Y((Type)0)
+		, Z((Type)0)
+	{}
+
+
+	Vector3 operator+(const Vector3& V)
+	{
+		return Vector3(this->X + V.X, this->Y + V.Y, this->Z + V.Z);
+	}
+
+	void operator+=(const Vector3& V)
+	{
+		this->X += V.X;
+		this->Y += V.Y;
+		this->Z += V.Z;
+	}
+
+	Vector3 operator-(const Vector3& V)
+	{
+		return Vector3(this->X - V.X, this->Y - V.Y, this->Z - V.Z);
+	}
+
+	Vector3 operator-()
+	{
+		return Vector3(-this->X, -this->Y, -this->Z);
+	}
+
+	void operator-=(const Vector3& V)
+	{
+		this->X -= V.X;
+		this->Y -= V.Y;
+		this->Z -= V.Z;
+	}
+
+	// Multiply by Scalar
+	Vector3 operator*(const Type& S)
+	{
+		return Vector3(this->X * S, this->Y * S, this->Z * S);
+	}
+
+	// Multiply by Scalar
+	void operator*=(const Type& S)
+	{
+		this->X *= S;
+		this->Y *= S;
+		this->Z *= S;
+	}
+
+	// Multiply by Vector(Dot)
+	Vector3 operator*(const Vector3& V)
+	{
+		return Vector3(this->X * V.X, this->Y * V.Y, this->Z * V.Z);
+	}
+
+	// Multiply by Vector(Dot)
+	void operator*=(const Vector3& V)
+	{
+		this->X *= V.X;
+		this->Y *= V.Y;
+		this->Z *= V.Z;
+	}
+
+	Vector3 operator/(const Type& S)
+	{
+		if (S != (Type)0)
+		{
+			return Vector3(this->X / S, this->Y / S, this->Z / S);
+		}
+		else
+		{
+			throw std::logic_error("Cannot divide by 0\n");
+		}
+	}
+
+	void operator/=(const Type& S)
+	{
+		if (S != (Type)0)
+		{
+			this->X /= S;
+			this->Y /= S;
+			this->Z /= S;
+		}
+		else
+		{
+			throw std::logic_error("Cannot divide by 0\n");
+		}
+	}
+
+	Type Length()
+	{
+		return sqrt(this->X * this->X
+				  + this->Y * this->Y
+				  + this->Z * this->Z);
+	}
+
+	Type LengthSquared()
+	{
+		return this->X * this->X
+			 + this->Y * this->Y
+			 + this->Z * this->Z;
+	}
+
+	// Returns normalized vector
+	Vector3 Normalize()
+	{
+		Type length = this->Length();
+		if (length != (Type)0)
+		{
+			return *this / length;
+		}
+		else
+		{
+			throw std::logic_error("Cannot divide by 0\n");
+		}
+	}
+
+	Type DotProduct(Vector3 V)
+	{
+		return this->X * V.X + this->Y * V.Y + this->Z * V.Z;
+	}
+
+	// Returns cross Vector of 2 Vectors
+	Vector3 Cross(const Vector3& V)
+	{
+		return Vector3(this->Y * V.Z - this->Z * V.Y,
+					   this->Z * V.X - this->X * V.Z,
+					   this->X * V.Y - this->Y * V.X);
+	}
+
+	// Returns Angle between two Vectors in Radians
+	// Works bad for integer types
+	static Type GetAngle(Vector3 V1, Vector3 V2)
+	{
+		return acos(V1.DotProduct(V2) / (V1.Length() * V2.Length()));
+	}
+
+	static Type DegToRad(Type Degrees)
+	{
+		return Degrees * ((Type)M_PI / (Type)180.0);
+	}
+
+	static Type RadToDeg(Type Radians)
+	{
+		return Radians * ((Type)180.0 / (Type)M_PI);
+	}
+
+	
+	// Print to console Vector in format (X, Y, Z)
+	void ToString()
+	{
+		std::cout << "(" << Round(X) << ", " << Round(Y) << ", " << Round(Z) << ")\n";
+	}
+
+
+	Type Round(Type Value, Type Precision = (Type)0.000001)
+	{
+		if (Precision == (Type)0)
+		{
+			Precision = (Type)1;
+		}
+		return std::round(Value / Precision) * Precision;
+	}
+};
---
+++ Task_3/Task_3/Task_3.cpp
@@ -0,0 +1,106 @@
+﻿#include "Source/Public/Quaternion.h"
+#include <iostream>
+typedef Vector3<float> Vec3f;
+typedef Vector3<double> Vec3;
+typedef Quaternion<double> Quat;
+typedef Quaternion<float> Quatf;
+
+int main()
+{
+	//==========ADD==========
+	{
+		Quat Q1(1.0);
+		Quat Q2(2.0);
+		std::cout << "==========ADD==========" << std::endl;
+		Q1.ToString();
+		std::cout << "+" << std::endl;
+		Q2.ToString();
+		std::cout << "=" << std::endl;
+		Quat Result = Q1 + Q2;
+		Result.ToString();
+		Q1 += Q2;
+		Q1.ToString();
+		std::cout << std::endl;
+	}
+	//==========SUBSTRACT==========
+	{
+		Quat Q1(1.0);
+		Quat Q2(2.0);
+		std::cout << "==========SUBSTRACT==========" << std::endl;
+		Q1.ToString();
+		std::cout << "-" << std::endl;
+		Q2.ToString();
+		std::cout << "=" << std::endl;
+		Quat Result = Q1 - Q2;
+		Result.ToString();
+		Q1 -= Q2;
+		Q1.ToString();
+		std::cout << std::endl;
+	}
+	//==========MULTIPLY==========
+	{
+		Quat Q1(1.0);
+		Quat Q2(2.0);
+		std::cout << "==========MULTIPLY==========" << std::endl;
+		Q1.ToString();
+		std::cout << "*" << std::endl;
+		Q2.ToString();
+		std::cout << "=" << std::endl;
+		Quat Result = Q1 * Q2;
+		Result.ToString();
+		Q1 *= Q2;
+		Q1.ToString();
+		std::cout << std::endl;
+	}
+	//==========DIVIDE==========
+	{
+		Quat Q1(1.0);
+		Quat Q2(2.0);
+		std::cout << "==========DIVIDE==========" << std::endl;
+		Q1.ToString();
+		std::cout << "/" << std::endl;
+		Q2.ToString();
+		std::cout << "=" << std::endl;
+		Quat Result = Q1 / Q2;
+		Result.ToString();
+		Q1 /= Q2;
+		Q1.ToString();
+		std::cout << std::endl;
+	}
+	//==========ROTATE==========
+	{
+		Vec3 V(-1.0);
+		double Angle = 270.0;
+		Vec3 Axis(1.0, 0.0, 0.0);
+		std::cout << "==========ROTATE==========" << std::endl;
+		V.ToString();
+		std::cout << "Rotated by " << Angle << " degrees, around" << std::endl;
+		Axis.ToString();
+		std::cout << "=" << std::endl;
+		Vec3 Result = Quat::Rotate(V, 270.0, Axis);
+		Result.ToString();
+		std::cout << std::endl;
+	}
+	//==========COMUTATIVITY==========
+	{
+		Quat Q1(2.0, 1.0, 2.0, 1.0);
+		Quat Q2(3.0);
+		std::cout << "==========COMUTATIVITY==========" << std::endl;
+		Q1.ToString();
+		std::cout << "*" << std::endl;
+		Q2.ToString();
+		std::cout << "=" << std::endl;
+		Quat Result = Q1 * Q2;
+		Result.ToString();
+		std::cout << "Reversed" << std::endl;
+		Q2.ToString();
+		std::cout << "*" << std::endl;
+		Q1.ToString();
+		std::cout << "=" << std::endl;
+		Result = Q2 * Q1;
+		Result.ToString();
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
